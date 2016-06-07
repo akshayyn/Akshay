a8255f8c8bd2248e07b857fbb63eebee7ea2b96c
Akshay <nakhawa.a@husky.neu.edu>
2016-06-07 13:11:19 -0700

    Employment Portal
    A Spring MVC based Employment Portal application.

---
+++ EmploymentPortal/src/main/java/org/akshay/EmploymentProtal/EmploymentPortal/App.java
@@ -0,0 +1,13 @@
+package org.akshay.EmploymentProtal.EmploymentPortal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
